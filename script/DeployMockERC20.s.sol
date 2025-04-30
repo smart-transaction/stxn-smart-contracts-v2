8aec07a958a6cfcb2eceeb62ab5b3fd4a53e245b
TokenTitan <r@stxn.io>
2025-04-30 17:51:19 +0530

    add: script to mint mock tokens to user addresses

--- script/DeployMockERC20.s.sol
+++ script/DeployMockERC20.s.sol
@@ -18,6 +18,40 @@ contract DeployMockERC20 is BaseDeployer {
         _deploy(_salt, deployerPrivateKey, name, symbol);
     }
 
+    function mintTokens(address mockERC20, address[] calldata users) external {
+        uint256 deployerPrivateKey = _getPrivateKey();
+
+        for (uint256 i = 0; i < networks.length; i++) {
+            NetworkConfig memory config = networks[i];
+            console.log("Minting on network:", config.name);
+
+            vm.createSelectFork(config.rpcUrl);
+            vm.startBroadcast(deployerPrivateKey);
+
+            MockERC20 token = MockERC20(mockERC20);
+            uint256 totalMinted = 0;
+
+            // Get token decimals and calculate base unit
+            uint8 decimals = token.decimals();
+            uint256 decimalsMultiplier = 10 ** decimals;
+
+            for (uint256 j = 0; j < users.length; j++) {
+                // Generate random amount between 1000 and 100000 token units
+                uint256 minAmount = 1000 * decimalsMultiplier;
+                uint256 maxAmount = 100000 * decimalsMultiplier;
+                uint256 amount = minAmount + (vm.randomUint() % (maxAmount - minAmount));
+
+                token.mint(users[j], amount);
+                totalMinted += amount;
+
+                console.log("Minted %s (10^%d) tokens to %s", amount / decimalsMultiplier, decimals, users[j]);
+            }
+
+            vm.stopBroadcast();
+            console.log("Total minted on %s: %s base units", config.name, totalMinted);
+        }
+    }
+
     function _deploy(bytes32 salt, uint256 deployerPrivateKey, string memory name, string memory symbol) internal {
         for (uint256 i = 0; i < networks.length; i++) {
             NetworkConfig memory config = networks[i];
@@ -27,7 +61,8 @@ contract DeployMockERC20 is BaseDeployer {
             vm.startBroadcast(deployerPrivateKey);
 
             address contractAddress = address(new MockERC20{salt: salt}(name, symbol));
-            address computedAddress = _computeCreate2Address(salt, hashInitCode(type(MockERC20).creationCode, abi.encode(name, symbol)));
+            address computedAddress =
+                _computeCreate2Address(salt, hashInitCode(type(MockERC20).creationCode, abi.encode(name, symbol)));
             require(contractAddress == computedAddress, "Contract address mismatch");
             console.log("MockERC20 deployed to:", contractAddress);
 
