3a2ba3397debf5502af239071e500fd45f9aab78
TokenTitan <r@stxn.io>
2025-08-27 11:21:13 +0530

    refactor: simplified KITN Disbursement contract

--- src/tests/KITNDisbursement.sol
+++ src/tests/KITNDisbursement.sol
@@ -1,58 +1,57 @@
 // SPDX-License-Identifier: BSL-1.
 pragma solidity 0.8.30;
 
-import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
-import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
-import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {IKITNToken} from "src/utils/interfaces/IKITNToken.sol";
 import {CallBreaker} from "src/CallBreaker.sol";
 import {DisbursalData} from "src/utils/interfaces/IKITNDisburement.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract KITNDisbursement is AccessControl, EIP712 {
-    bytes32 public constant DISBURSER_ROLE = keccak256("DISBURSER_ROLE");
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-
-    bytes32 private constant DISBURSAL_DATA_TYPEHASH = keccak256("DisbursalData(address[] receivers,uint256[] amounts)");
-
+contract KITNDisbursement is Ownable {
     IKITNToken public immutable KITNToken;
-    address public immutable callBreaker;
+    address public callBreaker;
 
-    /// @dev Error thrown when signature length is not equal to 65
-    /// @dev 0x4be6321b
-    error InvalidSignatureLength();
-    /// @dev Error thrown when a signature verification fails due to mismatch between recovered signer and expected signer
-    /// @dev 0x24ed5a32
-    /// @param recoveredAddress The address recovered from the signature
-    /// @param signer The expected signer address
-    error UnauthorisedSigner(address recoveredAddress, address signer);
     /// @dev Error thrown when address is zero
     /// @dev 0xd92e233d
     error ZeroAddress();
+
     /// @dev Error thrown when receive invalid data from callBreaker
     /// @dev 0xf0c7cf53
     error InvalidDataFromCallBreaker();
+
+    /// @dev Error thrown when caller is not the callBreaker
+    /// @dev 0x24ed5a32
+    /// @param caller The address of the caller
+    /// @param callBreaker The address of the callBreaker
+    error UnauthorisedCaller(address caller, address callBreaker);
 
     /// @notice Emited when Kitn Tokens are transfered to receivers
     /// @param receivers The addresses going to receive KITN Token
     /// @param amounts The amount of KITN Token to be received
     event TokensDisbursed(address[] indexed receivers, uint256[] amounts);
 
-    constructor(address _callBreaker, address _kitnToken, address _owner) EIP712("KITNDisbursement", "1") {
+    /// @notice Emited when callBreaker is updated
+    /// @param callBreaker The address of the new callBreaker
+    event CallBreakerUpdated(address indexed callBreaker);
+
+    modifier onlyCallBreaker() {
+        if (msg.sender != callBreaker) {
+            revert UnauthorisedCaller(msg.sender, callBreaker);
+        }
+        _;
+    }
+
+    constructor(address _callBreaker, address _kitnToken, address _owner) Ownable(_owner) {
         if (_callBreaker == address(0) || _kitnToken == address(0) || _owner == address(0)) {
             revert ZeroAddress();
         }
-
-        _grantRole(DEFAULT_ADMIN_ROLE, _owner);
-        _grantRole(ADMIN_ROLE, _owner);
-        _grantRole(DISBURSER_ROLE, _callBreaker);
 
         KITNToken = IKITNToken(_kitnToken);
         callBreaker = _callBreaker;
     }
 
     /// @notice verifies the validatorNodeSignature and transfer token to receivers
-    /// @dev Requires caller to have DISBURSER_ROLE
-    function disburseTokens() external onlyRole(DISBURSER_ROLE) {
+    /// @dev Requires caller to be the callBreaker
+    function disburseTokens() external onlyCallBreaker {
         // Retrieve data from CallBreaker's mevTimeStore
         bytes32 key = keccak256(abi.encodePacked("KITNDisbursalData"));
         bytes memory data = CallBreaker(payable(callBreaker)).mevTimeDataStore(key);
@@ -64,5 +63,13 @@ contract KITNDisbursement is AccessControl, EIP712 {
         DisbursalData memory disbursalData = abi.decode(data, (DisbursalData));
         KITNToken.batchMint(disbursalData.receivers, disbursalData.amounts);
         emit TokensDisbursed(disbursalData.receivers, disbursalData.amounts);
+    }
+
+    function setCallBreaker(address _callBreaker) external onlyOwner {
+        if (_callBreaker == address(0)) {
+            revert ZeroAddress();
+        }
+        callBreaker = _callBreaker;
+        emit CallBreakerUpdated(_callBreaker);
     }
 }
